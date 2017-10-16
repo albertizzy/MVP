64da47834e298cd21516a64fcad7a413c34e2d57
albertizzy <zydfh@163.com>
2017-10-16 15:41:06 +0800

    Initial commit

--- app/src/main/java/com/zy/mvp/main/widget/MainActivity.java
+++ app/src/main/java/com/zy/mvp/main/widget/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         mMainPresenter.switchNavigation(id);
+        item.setChecked(true);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
