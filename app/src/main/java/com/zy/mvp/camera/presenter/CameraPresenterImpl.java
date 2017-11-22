d43c05b19577998ca5b5714948ac9e09d82e16d8
albertizzy <zydfh@163.com>
2017-11-22 15:15:37 +0800

    Initial commit

--- app/src/main/java/com/zy/mvp/camera/presenter/CameraPresenterImpl.java
+++ app/src/main/java/com/zy/mvp/camera/presenter/CameraPresenterImpl.java
@@ -2,12 +2,21 @@ package com.zy.mvp.camera.presenter;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import com.zy.mvp.camera.view.CameraView;
 import com.zy.mvp.camera.widget.CameraFragment;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class CameraPresenterImpl implements CameraPresenter {
     private CameraView mListView;
@@ -32,7 +41,7 @@ public class CameraPresenterImpl implements CameraPresenter {
     }
 
     private void success(final List<String> list) {
-//TODO 模拟延迟加载
+//TODO 正常加载
 //        handler.post(new Runnable() {
 //            @Override
 //            public void run() {
@@ -40,13 +49,33 @@ public class CameraPresenterImpl implements CameraPresenter {
 //                mListView.addData(list);
 //            }
 //        });
-        handler.postDelayed(new Runnable() {
+// TODO 模拟延迟加载
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                mListView.hideProgress();
+//                mListView.addData(list);
+//            }
+//        }, 1000);
+//FIXME Rxjava
+        Observable.create(new ObservableOnSubscribe<List<String>>() {
             @Override
-            public void run() {
-                mListView.hideProgress();
-                mListView.addData(list);
+            public void subscribe(ObservableEmitter<List<String>> emitter) throws Exception {
+                Thread.sleep(1000);//FIXME Rxjava模拟延迟加载，正式可删
+                emitter.onNext(list);
+                emitter.onComplete();
             }
-        }, 1000);
+        })
+                .subscribeOn(Schedulers.io())//设置可观察对象在Schedulers.io()的线程中发射数据
+                .observeOn(AndroidSchedulers.mainThread())//设置观察者在当前线程中接收数据
+                .subscribe(new Consumer<List<String>>() {
+                    @Override
+                    public void accept(@NonNull List<String> list) throws Exception {
+                        Log.e("sdf", "asdasd");
+                        mListView.hideProgress();
+                        mListView.addData(list);
+                    }
+                });
     }
 
     private void failure(final String message) {
