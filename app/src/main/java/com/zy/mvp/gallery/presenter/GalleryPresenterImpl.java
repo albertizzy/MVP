a670ed44c47cf09e15a3a32075d5c997568f769e
albertizzy <zydfh@163.com>
2017-11-23 12:19:52 +0800

    Initial commit

--- app/src/main/java/com/zy/mvp/gallery/presenter/GalleryPresenterImpl.java
+++ app/src/main/java/com/zy/mvp/gallery/presenter/GalleryPresenterImpl.java
@@ -34,7 +34,7 @@ public class GalleryPresenterImpl implements GalleryPresenter {
         //只有第一页的或者刷新的时候才显示刷新进度条
 // FIXME Rxjava 替换了下面8行
 //        if (page == 1) {
-//            mListView.showProgress();
+// FIXME move to doOnSubscribe           mListView.showProgress();
 //        }
 //        List<String> list = new ArrayList<>();
 //        for (int i = 0; i < GalleryFragment.PAGE_SIZE; i++) {
@@ -54,19 +54,11 @@ public class GalleryPresenterImpl implements GalleryPresenter {
                 emitter.onNext(list);
                 emitter.onComplete();
             }
-        })
-                .doOnSubscribe(new Consumer<Disposable>() {
-                    @Override
-                    public void accept(Disposable disposable) throws Exception {
-                        if (page == 1) {
-                            mListView.showProgress();
-                        }
-                    }
-                });
-        success(observable);
+        });
+        success(observable, page);
     }
 
-    private void success(Observable<List<String>> observable) {
+    private void success(Observable<List<String>> observable, final int page) {
         observable
                 .subscribeOn(Schedulers.io())//设置可观察对象在Schedulers.io()的线程中发射数据
                 // （用于IO密集型的操作，例如读写SD卡文件，查询数据库，访问网络等，
@@ -74,6 +66,14 @@ public class GalleryPresenterImpl implements GalleryPresenter {
                 // 是否有空闲的线程，如果有，则复用，如果没有则创建新的线程，并加入到线程池中，如果每次都没有空闲线程使用，可以无上限的创建新线程）
                 .observeOn(AndroidSchedulers.mainThread())//设置观察者在当前线程中接收数据
                 // （在Android UI线程中执行任务，为Android开发定制）
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Exception {
+                        if (page == 1) {
+                            mListView.showProgress();
+                        }
+                    }
+                })
                 .doFinally(new Action() {
                     @Override
                     public void run() throws Exception {
@@ -84,13 +84,14 @@ public class GalleryPresenterImpl implements GalleryPresenter {
                     @Override
                     public void accept(@NonNull List<String> list) throws Exception {
                         Log.e("accept", "execute");
-// FIXME doFinally                       mListView.hideProgress();
+// FIXME move to doFinally                       mListView.hideProgress();
                         mListView.addData(list);
                     }
                 }, new Consumer<Throwable>() {
                     @Override
                     public void accept(Throwable throwable) throws Exception {
                         mListView.showLoadFailMsg("失败");
+                        Log.e("Throwable", throwable.getMessage());
                     }
                 });
     }
