960f984ceeb1d88a311b19a782b00e20ad439476
albertizzy <zydfh@163.com>
2017-12-12 23:58:13 +0800

    Initial commit

--- app/src/main/java/com/zy/mvp/tab/widget/TabFragment.java
+++ app/src/main/java/com/zy/mvp/tab/widget/TabFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +11,6 @@ import android.view.ViewGroup;
 
 import com.zy.mvp.R;
 import com.zy.mvp.camera.widget.CameraFragment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class TabFragment extends Fragment {
     private TabLayout mTablayout;
@@ -39,40 +34,11 @@ public class TabFragment extends Fragment {
 
     private void setupViewPager(ViewPager mViewPager) {
         //Fragment中嵌套使用Fragment一定要使用getChildFragmentManager(),否则会有问题
-        MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getChildFragmentManager());
         adapter.addFragment(CameraFragment.newInstance("", true), "一");
         adapter.addFragment(CameraFragment.newInstance("", true), "二");
         adapter.addFragment(CameraFragment.newInstance("", true), "三");
         adapter.addFragment(CameraFragment.newInstance("", true), "四");
         mViewPager.setAdapter(adapter);
-    }
-
-    public static class MyPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragments = new ArrayList<>();
-        private final List<String> mFragmentTitles = new ArrayList<>();
-
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragments.add(fragment);
-            mFragmentTitles.add(title);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragments.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragments.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitles.get(position);
-        }
     }
 }
