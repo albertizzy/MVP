eb412f808455e61ebb38ba0a4026622e71261e2b
albertizzy <zydfh@163.com>
2017-12-13 10:05:01 +0800

    Initial commit

--- app/src/main/java/com/zy/mvp/share/widget/ShareFragment.java
+++ app/src/main/java/com/zy/mvp/share/widget/ShareFragment.java
@@ -1,5 +1,6 @@
 package com.zy.mvp.share.widget;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zy.mvp.R;
@@ -17,6 +19,7 @@ import com.zy.mvp.share.presenter.SharePresenter;
 import com.zy.mvp.share.presenter.SharePresenterImpl;
 import com.zy.mvp.share.view.ShareView;
 import com.zy.mvp.utils.DividerItemDecoration;
+import com.zy.mvp.utils.touchhelper.ItemTouchHelperAdapter;
 import com.zy.mvp.utils.touchhelper.SimpleItemTouchHelperCallback;
 
 import java.util.ArrayList;
@@ -182,5 +185,134 @@ public class ShareFragment extends Fragment implements ShareView {
     public void onPause() {
         super.onPause();
         mListPresenter.unsubscribe();
+    }
+
+    private static class ShareRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements ItemTouchHelperAdapter {
+        private List<String> mData;
+        private final Context context;
+        private static final int TYPE_ITEM = 0;
+        private static final int TYPE_FOOTER = 1;
+        private boolean mShowFooter = false;
+
+        public ShareRecyclerViewAdapter(Context context) {
+            this.context = context;
+        }
+
+        public void setmData(List<String> mData) {
+            this.mData = mData;
+            this.notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            // 最后一个item设置为footerView
+            if (!mShowFooter) {
+                return TYPE_ITEM;
+            }
+            if (position + 1 == getItemCount()) {
+                return TYPE_FOOTER;
+            } else {
+                return TYPE_ITEM;
+            }
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            if (viewType == TYPE_ITEM) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.recycler_item, parent, false);
+                return new ViewHolder(view);
+            } else {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.fragment_footer, null);
+                view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+                return new FooterViewHolder(view);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+            if (holder instanceof ViewHolder) {
+                final ViewHolder viewHolder = (ViewHolder) holder;
+                viewHolder.mItemId.setText((position + 1) + "");
+                viewHolder.mItemName.setText(mData.get(position));
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mOnItemClickLitener.onItemClick(v, holder.getLayoutPosition());
+                    }
+                });
+//            viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+//                @Override
+//                public boolean onLongClick(View v) {
+//                    mOnItemClickLitener.onItemLongClick(v, holder.getLayoutPosition());
+//                    return false;
+//                }
+//            });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            int begin = mShowFooter ? 1 : 0;
+            if (mData == null) {
+                return begin;
+            }
+            return mData.size() + begin;
+        }
+
+        public void isShowFooter(boolean showFooter) {
+            this.mShowFooter = showFooter;
+        }
+
+        public boolean isShowFooter() {
+            return this.mShowFooter;
+        }
+
+        public class FooterViewHolder extends RecyclerView.ViewHolder {
+            public FooterViewHolder(View view) {
+                super(view);
+            }
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mItemId;
+            public TextView mItemName;
+
+            public ViewHolder(View view) {
+                super(view);
+                mItemId = view.findViewById(R.id.itemid);
+                mItemName = view.findViewById(R.id.itemname);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mItemName.getText() + "'";
+            }
+        }
+
+        public interface OnItemClickLitener {
+            void onItemClick(View view, int position);
+
+//        void onItemLongClick(View view, int position);
+        }
+
+        private OnItemClickLitener mOnItemClickLitener;
+
+        public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
+            this.mOnItemClickLitener = mOnItemClickLitener;
+        }
+//    @Override
+//    public void onItemMove(int fromPosition, int toPosition) {
+//        Collections.swap(mData, fromPosition, toPosition);
+//        notifyItemMoved(fromPosition, toPosition);
+//    }
+
+        @Override
+        public void onItemDissmiss(int position) {
+            mData.remove(position);
+            notifyItemRemoved(position);
+        }
     }
 }
