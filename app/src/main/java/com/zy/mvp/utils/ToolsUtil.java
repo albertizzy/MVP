eedd31067c44449b281ed8e6a814519f71d9d8af
albertizzy <zydfh@163.com>
2017-12-27 10:02:23 +0800

    lint

--- app/src/main/java/com/zy/mvp/utils/ToolsUtil.java
+++ app/src/main/java/com/zy/mvp/utils/ToolsUtil.java
@@ -1,8 +1,6 @@
 package com.zy.mvp.utils;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 
 public class ToolsUtil {
     public static int getHeightInPx(Context context) {
@@ -61,29 +59,5 @@ public class ToolsUtil {
             e.printStackTrace();
         }
         return statusHeight;
-    }
-
-    /**
-     * 判断网络是否可用
-     *
-     * @param context
-     * @return
-     */
-    public static boolean isNetworkAvailable(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (cm != null) {
-            //如果仅仅是用来判断网络连接
-            //则可以使用 cm.getActiveNetworkInfo().isAvailable();
-            NetworkInfo[] info = cm.getAllNetworkInfo();
-            if (info != null) {
-                for (NetworkInfo networkInfo : info) {
-                    if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
     }
 }
